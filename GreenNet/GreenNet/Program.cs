8237110d4fabbbbc2ad34192833049d6d66e70f4
GreenCommunications <alexander.m.rankin@gmail.com>
2016-06-27 18:14:22 -0400

    First

---
+++ GreenNet/GreenNet/Program.cs
@@ -0,0 +1,25 @@
+﻿//WalkWay (C) 2016
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace GreenNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("GreenNet Server Windows ALPHA V1.0.0");
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
